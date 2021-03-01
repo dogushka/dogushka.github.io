c998ae0385126719649ecb8bb1cb78b3210898d4
dogushka <76619406+dogushka@users.noreply.github.com>
2021-03-01 19:01:30 +0300

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -113,3 +113,30 @@ $("#news-slider2").owlCarousel({
     navigationText:false,
     autoPlay:true
 });
+
+function $_GET(key) {
+    let p = window.location.search;
+    p = p.match(new RegExp(key + "=([^&=]+)"));
+    return p ? Boolean(p[1]) : false;
+}
+
+function color() {
+    let head  = document.getElementsByTagName("head")[0];
+    let link  = document.createElement("link");
+    link.id   = "css";
+    link.rel  = "stylesheet";
+    link.type = "text/css";
+    link.media = "all";
+    if($_GET("white")) {
+        link.href = "./white.css";
+    } else {
+        link.href = "./black.css";        
+    }
+    head.appendChild(link);
+}
+
+var cpCreditsUrl = "https://codepen.io/PickJBennett/details/BdbrMW";
+var cpCreditsTitle = "🌈 Sexy Animated Rainbow Waves Header";
+var cpCreditsTwitter = document.getElementById("js-tweet-this");
+cpCreditsTwitter.href = "https://twitter.com/intent/tweet?text=" + encodeURI(cpCreditsTitle) + "&url=" + encodeURI(cpCreditsUrl) + "&via=PickJBennett&related=PickJBennett,CiaoFileManager";
+cpCreditsTwitter.innerHTML += "Tweet This Pen";
